e051781a124eebedf2f402537d9524fed1248d10
ouhe <ou.heng@gmail.com>
2021-09-14 08:27:57 +0100

    set state

--- src/App/App.js
+++ src/App/App.js
@@ -4,21 +4,31 @@ import './App.css';
 import Button from './components/Button/Button';
 
 class App extends React.Component {
-  counter = 0
+  // counter = 0
+  constructor(props){
+    super(props);
+    this.state={counter:0};
+  }
+  
   render() {
     return (
       <div className="App">
-        Valeur du counter : {this.counter}
+        Valeur du counter : {this.state.counter}
         <hr />
         <Button onClickEvent={() => {
           console.log(this);
-          this.counter--;
-          console.log(this.counter);
+          // cannot change state value directly by state.counter++
+          // since it's not mutable, 
+          // setState will create another immutable object and refresh UI
+          // that's why we can see the change of "Valeur du counter"
+          this.setState({counter:this.state.counter - 1});
+          console.log(this.state.counter);
         }}>soustraction</Button>
         <Button onClickEvent={() => {
           console.log(this);
-          this.counter++;
-          console.log(this.counter);
+          // asynchro
+          this.setState({counter:this.state.counter + 1});
+          console.log(this.state.counter);
         }} bgColor="chartreuse">addition</Button>
       </div>
     );
