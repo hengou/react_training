2759b9c03f6dc0bc41838556200695596dbefe71
ouhe <ou.heng@gmail.com>
2021-09-14 15:02:29 +0100

    add custom select user

--- src/App/components/FormMessage/FormMessage.js
+++ src/App/components/FormMessage/FormMessage.js
@@ -1,16 +1,24 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './FormMessage.module.scss';
+// import Select from 'react-select';
+import SelectUser from '../SelectUser/SelectUser'
+import { userListInitialState } from '../UserList/UserList'
 
 export const formMessageInitialState = {};
 
 const FormMessage = (props) => {
   const [formMessageState, setFormMessagestate] = useState(formMessageInitialState);
-  
+
   return (
     <div className={styles.FormMessage} data-testid="FormMessage">
-      FormMessage Component
-      {JSON.stringify(formMessageState)}
+      <SelectUser users={userListInitialState} />
+      {/* <Select options={userListInitialState.map((e, i) => {
+        var rObj = {};
+        rObj.value = e.id; 
+        rObj.label = e.nickname;
+        return rObj;
+      })} /> */}
     </div>
   );
 }
---
+++ src/App/components/SelectUser/SelectUser.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './SelectUser.module.scss';
+
+const SelectUser = (props) => (
+  <select className={styles.SelectUser} data-testid="SelectUser" onChange={props.onChange}>
+    {/* key used technically by react */}
+    {props.users.map((e, i) => <option value={e.id} key={'opt-sur-' + i}>{e.nickname}</option>)}
+  </select>
+);
+
+SelectUser.propTypes = {
+  users: PropTypes.array.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+SelectUser.defaultProps = {};
+
+export default SelectUser;
